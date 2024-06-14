95ac1665f912b05c6b253ac2d8fc86c352982e7d
Muhammad Tayyab Roheen <tayyabroheen@gmail.com>
2024-06-15 02:08:22 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+// Initialization for ES Users
+import {
+    Carousel,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Carousel });
